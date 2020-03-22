b7c2266ba9d7748cb1ee6f84592ba69795f7de68
praizerema <eremapraiz@gmail.com>
2020-03-22 18:34:13 +0100

    my task

---
+++ javascripttask1/function.js
@@ -0,0 +1,22 @@
+let name= "Ayodele Praise";
+document.getElementById("name").innerHTML = name;
+let courses= ["PHP", "Python", "nodeJs", "HTML-CSS-JS"],
+
+ol = document.createElement('ol');
+
+document.getElementById('courses').appendChild(ol);
+
+courses.forEach(function (course) {
+    let li = document.createElement('li');
+    ol.appendChild(li);
+
+    li.innerHTML += course;
+});
+
+var arrEven=[]
+for(var i = 1; i<=200; i++){
+    if(i % 2 === 0){
+        arrEven.push(i)
+    }
+}
+document.getElementById("even").innerHTML = arrEven;
